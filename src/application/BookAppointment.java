f68a9598e59769d2a74e15acc500e3892fec5113
JonathanBlairMCE <jonathan.blair@macys.com>
2016-02-18 10:44:59 -0500

    Appointment page
    has been added

---
+++ src/application/BookAppointment.java
@@ -0,0 +1,97 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+public class BookAppointment extends Application {
+	
+	static Stage window;
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+	    
+		window = primaryStage;
+		
+	    // constructing our scene
+	    URL url = getClass().getResource("BookAppointment.fxml");
+	    AnchorPane pane = FXMLLoader.load(url);
+	    Scene BookAppointment = new Scene(pane);
+	    
+	    // setting the stage
+	    window.setScene(BookAppointment);
+	    window.setTitle("Book Appointment");
+	    window.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@FXML
+    private ComboBox<?> ddTime;
+
+    @FXML
+    private ComboBox<?> ddBudget;
+
+    @FXML
+    private ComboBox<?> ddDepartment;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    private ComboBox<?> ddFloor;
+
+    @FXML
+    private TextArea taSelfDescription;
+
+    @FXML
+    private Button btSubmit;
+
+    @FXML
+    private ComboBox<?> ddGender;
+
+    @FXML
+    private ToggleButton tgPreselection;
+
+    @FXML
+    private DatePicker dtDate;
+
+    @FXML
+    private TextArea taLookingFor;
+
+    @FXML
+    private ToggleButton tgAppointment;
+
+    @FXML
+    private ComboBox<?> ddBodyType;
+
+    @FXML
+    void futureAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bookAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void outfitPreselection(ActionEvent event) {
+
+    }
+
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -3,28 +3,61 @@ package application;
 import java.io.IOException;
 import java.net.URL;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    static Stage window;
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-    
+		
+		window = primaryStage;
 	    // constructing our scene
 	    URL url = getClass().getResource("Index.fxml");
 	    AnchorPane pane = FXMLLoader.load(url);
-	    Scene scene = new Scene(pane);
+	    Scene Main = new Scene(pane);
 	    
 	    // setting the stage
-	    primaryStage.setScene(scene);
-	    primaryStage.setTitle("Macys Helper");
-	    primaryStage.show();
+	    window.setScene(Main);
+	    window.setTitle("Macys Helper");
+	    window.show();
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+    public void goToBookAppointment() {
+        try{
+            new BookAppointment().start(window);
+        } catch (Exception e) {}
+        System.out.println("It's connected");
+    }
+	
+	@FXML
+    private TextField testTF;
+
+    @FXML
+    private Button testButton;
+    
+    @FXML
+    private Button getHelpButton;
+
+    @FXML
+    void testAction(ActionEvent event) {
+    	testTF.setText("Let's make this happen!");
+    }
+    
+    @FXML
+    void getHelp(ActionEvent event) {
+    	goToBookAppointment();
+    }
 }
