009431c63215da4f691751166221c8669a8ad686
SpartainStudio <andrew.dessin@gmail.com>
2016-02-18 09:09:57 -0500

    Added Customer Class
    Added Customer Class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
-	
+
 import java.io.IOException;
+import application.Domain.*;
 import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,21 +10,21 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-    
+
 	    // constructing our scene
 	    URL url = getClass().getResource("Index.fxml");
 	    AnchorPane pane = FXMLLoader.load(url);
 	    Scene scene = new Scene(pane);
-	    
+
 	    // setting the stage
 	    primaryStage.setScene(scene);
 	    primaryStage.setTitle("Macys Helper");
 	    primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
